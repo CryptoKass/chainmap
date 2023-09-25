1891b5145987cf90d26d7c4db03c81c86a2566c3
KassCrypto <kasscrypto@gmail.com>
2023-09-25 19:17:33 +0100

    sepolia + nova + extra rpcs added

--- data/arbitrum.list.ts
+++ data/arbitrum.list.ts
@@ -20,6 +20,25 @@ export default {
     rpc: {
       Arbitrum: "https://goerli-rollup.arbitrum.io/rpc",
       PublicNode: "https://arbitrum-goerli.publicnode.com",
+      Blast: "https://arbitrum-goerli.public.blastapi.io",
+    },
+  },
+
+  sepolia: {
+    chainId: 421614,
+    name: "Arbitrum sepolia",
+    rpc: {
+      Arbitrum: "https://sepolia-rollup.arbitrum.io/rpc",
+    },
+  },
+
+  nova: {
+    chainId: 42170,
+    name: "Arbitrum nova",
+    rpc: {
+      Arbitrum: "https://nova.arbitrum.io/rpc",
+      PublicNode: "https://arbitrum-nova.publicnode.com",
+      Blast: "https://arbitrum-nova.public.blastapi.io",
     },
   },
 } as ChainMap;
