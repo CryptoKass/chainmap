9cf58fef27592763401071b051ff6fcf0f635b41
Bot <cryptokass@users.noreply.github.com>
2023-09-26 11:11:08 +0000

    🧹 chore: generate & build

--- dist/ethereum.js
+++ dist/ethereum.js
@@ -17,6 +17,7 @@ var mainnetInfo = {
         "https://eth.llamarpc.com",
         "https://eth-mainnet.public.blastapi.io",
         "https://rpc.ankr.com/eth",
+        "https://rpc.flashbots.net/",
         "https://cloudflare-eth.com"
     ]
 };
@@ -51,7 +52,8 @@ var sepoliaInfo = {
         "https://rpc.sepolia.org",
         "https://rpc2.sepolia.org",
         "https://eth-sepolia.public.blastapi.io",
-        "https://rpc.notadegen.com/eth/sepolia"
+        "https://rpc.notadegen.com/eth/sepolia",
+        "https://gateway.tenderly.co/public/sepolia"
     ]
 };
 exports.default = {
